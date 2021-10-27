54c52cb8b1ed610346bc9317915185e0cc676aae
Wasiim Hosenbocus <wasiim.hosenbocus@outlook.com>
2021-10-27 16:04:11 +0400

    Future Proof API

--- src/eShop.Catalog.API/Controllers/ProductController.cs
+++ src/eShop.Catalog.API/Controllers/ProductController.cs
@@ -1,7 +1,7 @@
 using AutoMapper;
 using Microsoft.AspNetCore.Mvc;
 
-[Route("api/[controller]")]
+[Route("api/Catalog/[controller]")]
 [ApiController]
 public class ProductController : ControllerBase
 {
